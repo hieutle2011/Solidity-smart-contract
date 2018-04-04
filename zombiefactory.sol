7581e09af419685fa5125118686e46ed836c3a96
ltrunghieu <ltrunghieu@tma.com.vn>
2018-04-04 12:50:31 +0700

    update

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -9,6 +9,8 @@ contract ZombieFactory is Ownable {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    uint cooldownTime = 1 days;
+
     struct Zombie {
         string name;
         uint dna;
@@ -22,7 +24,7 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
